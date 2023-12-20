57273d5f77f695d3ed1e9d6d3d7b3952931d137c
Joy097 <shihabrumeechowdhury@gmail.com>
2023-12-21 00:46:06 +0600

    update 66

---
+++ Java Exam portal/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+import org.json.simple.JSONObject;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+
+public class Main {
+    public static void main(String[] args) {
+        JSONObject js = new JSONObject();
+        js.put("name", "test");
+        js.put("password", "test");
+        System.out.println(js);
+    }
+}
