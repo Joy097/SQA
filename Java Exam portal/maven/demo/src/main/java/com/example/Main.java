df7326db9197608f4680c5b4b411abe7ca678e39
Joy097 <shihabrumeechowdhury@gmail.com>
2023-12-21 00:21:28 +0600

    update 43

--- Java Exam portal/maven/demo/src/main/java/com/example/Main.java
+++ Java Exam portal/maven/demo/src/main/java/com/example/Main.java
@@ -1,7 +1,11 @@
 package com.example;
+import org.json.simple.parser.JSONParser;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JSONParser js = new JSONObject();
+        js.put("name", "test");
+        js.put("password", "test");
+        System.out.println(js);
     }
 }
